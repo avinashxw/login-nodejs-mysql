20a1532a6e8be5e48acd18fe7007e90d8e073731
Avinash Wadhawan <avinashxw@gmail.com>
2021-05-11 23:22:10 +0530

    db-connected

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require('express')
 const mysql = require('mysql')
+const bodyparser = require('body-parser')
 
 const app = express()
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'loginnodemysql'
+})
+
+db.connect((err,res) => {
+    if(err) {
+        throw err
+        console.log(err)
+    }
+    else {
+        console.log('DB connected!')
+    }
+})
 
 app.get('/', (req,res) => {
     res.send('<h1>Welcome to the world of programming!</h1>')
