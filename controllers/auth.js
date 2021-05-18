7eef5a4c85aa9f6bdc27b9fe2c4347d594aa08ce
Avinash Wadhawan <avinashxw@gmail.com>
2021-05-18 23:30:31 +0530

    login-validation-incorrect-check

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,19 +2,18 @@ const mysql = require('mysql')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 
-exports.register = (req,res) => {
-    console.log(req.body)
+const db = mysql.createConnection({
+    host: process.env.HOSTNAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASENAME
+})
 
+exports.register = (req,res) => {
+    //console.log(req.body)
     const {
         name, email, password, passwordconfirm
     } = req.body
-
-    const db = mysql.createConnection({
-        host: process.env.HOSTNAME,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DATABASENAME
-    })
 
     db.query('SELECT email from tbl_user WHERE email = ?', [email], async(err,result) => {
         if(err) {
@@ -49,4 +48,33 @@ exports.register = (req,res) => {
         })
         
     })
+}
+
+exports.login = async (req,res) => {
+
+    try {
+        const { email, password  } = req.body
+        console.log(req.body)
+        if(!email || !password) {
+            return res.status(400).render('signin', {
+                message: `Please input an email address & password to proceed!`
+            })
+        }
+
+        db.query('SELECT * FROM tbl_user WHERE email = ?', [email], async (error, result) => {
+            console.log(result)
+            if(!result || (!await bcrypt.compare(password, result[0].password))) {
+                res.status(401).render('signin', {
+                    message: `The email address or password is incorrect!`
+                })
+            }
+            else {
+                const id = result[0].id
+                
+            }
+        })
+
+    } catch(error) {
+        console.log(error)
+    }
 }
